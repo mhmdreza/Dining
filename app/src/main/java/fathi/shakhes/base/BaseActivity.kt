588bae1c442f054408b05c7ce90b1f477e566c07
mohamadreza <mohamadreza.jafarzade@hezardastan.net>
2021-09-04 20:40:21 +0430

    Add base classes for fragment and activity.

---
+++ app/src/main/java/fathi/shakhes/base/BaseActivity.kt
@@ -0,0 +1,27 @@
+package fathi.shakhes.base
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import shakhes.R
+
+abstract class BaseActivity : AppCompatActivity()
+
+fun Activity.openFragment(fragment: Fragment, addToBackStack: Boolean) {
+    if (this !is AppCompatActivity || isFinishing) return
+    val transaction = supportFragmentManager
+        .beginTransaction()
+        .replace(R.id.container, fragment)
+
+    if (addToBackStack) {
+        transaction.addToBackStack(fragment.javaClass.name)
+    }
+    transaction.commit()
+}
+
+fun Activity.openBottomSheet(bottomSheetDialogFragment: BottomSheetDialogFragment) {
+    if (this !is AppCompatActivity || isFinishing) return
+
+    bottomSheetDialogFragment.show(supportFragmentManager, "bottomSheet")
+}
---
+++ app/src/main/java/fathi/shakhes/base/BaseFragment.kt
@@ -0,0 +1,27 @@
+package fathi.shakhes.base
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BaseFragment : Fragment() {
+}
+
+fun Fragment.navigateBack() {
+    parentFragmentManager.popBackStack()
+}
+
+fun Fragment.openFragment(fragment: Fragment, addToBackStack: Boolean) {
+    activity?.openFragment(fragment, addToBackStack)
+}
+
+fun Fragment.openBottomSheet(bottomSheetDialogFragment: BottomSheetDialogFragment) {
+    if (activity == null || activity?.isFinishing == true) return
+
+    bottomSheetDialogFragment.show(parentFragmentManager, "bottomSheet")
+}
+
+
+fun Fragment.setSupportFragmentResult(requestKey: String, result: Bundle = Bundle()) {
+    activity?.supportFragmentManager?.setFragmentResult(requestKey, result)
+}
